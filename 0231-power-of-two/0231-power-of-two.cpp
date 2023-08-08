354beae9477a0a04b349538b121e97f9912e0b29
Aditya Pandey <aditya.pandey.241103@gmail.com>
2023-08-08 12:51:14 +0530

    Time: 5 ms (16.73%), Space: 6 MB (39.31%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)
+            return false;
+        
+        int ans = n&(n-1);
+        
+        
+        if (ans==0)
+            return true;
+        
+        else 
+            return false;
+    }
+};
