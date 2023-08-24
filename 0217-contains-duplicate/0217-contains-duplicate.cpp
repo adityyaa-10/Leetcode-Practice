480594d8950508fc4d7cf326c177f1743c6f0253
Aditya Pandey <aditya.pandey.241103@gmail.com>
2023-08-24 21:57:52 +0530

    Time: 96 ms (82.78%), Space: 69.6 MB (49.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,31 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         long long int size = nums.size() - 1;
+//         for (long long int i = 0; i < size; i++) 
+//         {
+//             if (nums[i] == nums[i + 1])
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        long long int size = nums.size() - 1;
-        for (long long int i = 0; i < size; i++) 
-        {
-            if (nums[i] == nums[i + 1])
-            {
+        unordered_set<int> s;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) {
                 return true;
             }
+            s.insert(nums[i]);
         }
+        
         return false;
     }
 };
