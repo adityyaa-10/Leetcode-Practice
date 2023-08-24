987a647391dbb6b00acbf933e88c2d35525fe1f0
Aditya Pandey <aditya.pandey.241103@gmail.com>
2023-08-24 21:55:32 +0530

    Time: 88 ms (92.64%), Space: 57.3 MB (74.33%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        long long int size = nums.size() - 1;
+        for (long long int i = 0; i < size; i++) 
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
