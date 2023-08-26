93c162ce2b5e6c7ec3079c9bb82001568261adaf
Aditya Pandey <aditya.pandey.241103@gmail.com>
2023-08-26 20:54:48 +0530

    Time: 9 ms (97.92%), Space: 23.3 MB (28.40%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,9 +1,13 @@
-
-
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n=s.size();
-        reverse(s.begin(),s.begin()+n);
+        int n = s.size();
+        int start =0;
+        int end =n-1;
+        
+        while(start<end)
+        {
+            swap(s[start++], s[end--]);
+        }
     }
 };
