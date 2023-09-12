d2973cf2dfbee5b286e09a8ccd557ec413f632aa
Aditya Pandey <aditya.pandey.241103@gmail.com>
2023-09-12 21:39:03 +0530

    Time: 5 ms (9.55%), Space: 8.2 MB (49.97%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        // Using selection sort
+        for(int i = 0 ; i< nums.size()-1 ; i++){
+            int minIndex = i;
+            for(int j = i+1; j<nums.size() ; j++)
+            {
+                if(nums[j] < nums[minIndex])
+                    minIndex = j;
+            }
+            swap(nums[minIndex], nums[i]);
+        }
+    }
+};
